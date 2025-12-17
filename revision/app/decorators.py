2aef7ad2a4f623985c5077a65b9d8f9d0f7a9735
“Maria <milashkmilash@gmail.com>
2025-12-17 10:30:14 +0200

    I added decorator

--- revision/app/decorators.py
+++ revision/app/decorators.py
@@ -1,6 +1,6 @@
+"""
 import datetime
 import time
-"""
 def timer(func):
     def wrapper(*args, **kwargs):
         start = datetime.datetime.now()
@@ -23,8 +23,15 @@ def round_result(ndigits: int):
     def decorator(func):
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
-            if isinstance(result(int, float)):
+            if isinstance(result, (int, float)):
                 return round(result, ndigits)
             return result
         return wrapper
     return decorator
+
+@round_result(2)
+def calc(x):
+    return x
+
+print(calc(12.432456))
+print(calc(12.34))
